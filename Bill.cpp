6aeb0a721b79bb2d77b08129fa69165286d75b14
bhupeshsinha <bhupeshsinha247@gmail.com>
2024-11-08 11:55:13 +0530

    Created the object of the class and asked query from the user

--- Bill.cpp
+++ Bill.cpp
@@ -38,6 +38,24 @@ class Bill
 
 int main()
 {
+    Bill b;
+    bool exit = false;      //variable that terminate the while loop
+
+    while(!exit)
+    {
+        system("cls");          //this statement clear the previous output and display only the current output
+
+        int val;
+
+        cout<<"\t Welcome to Super Market Billing System"<<endl;
+        cout<<"\t **************************************"<<endl;
+        cout<<"\t 1. Add Item."<<endl;
+        cout<<"\t 2. Print Bill."<<endl;
+        cout<<"\t 3. Exit."<<endl;
+        cout<<"\t\t Enter Choice: ";
+        cin>>val;
+
+    }
 
     return 0;
 }
