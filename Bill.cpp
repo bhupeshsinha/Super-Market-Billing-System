5eb361c9f4485096f9a91b11db0679c6b55a65cf
bhupeshsinha <bhupeshsinha247@gmail.com>
2024-11-08 11:35:24 +0530

    Added class Bill definition

--- Bill.cpp
+++ Bill.cpp
@@ -4,3 +4,40 @@
 #include<windows.h>
 
 using namespace std;
+
+class Bill
+{
+      
+    private:
+            string item;
+            int rate, quantity;
+    
+    public:
+
+            Bill(): item(""), rate(0), quantity(0){}
+
+    void setItem(string item)            
+    {
+        this->item=item;
+    }
+
+    void setRate(string rate)            
+    {
+        this->rate=rate;
+    }
+
+        void setQuantity(string quantity)            
+    {
+        this->quantity=quantity;
+    }
+
+    string getItem()    return this->item;
+    int getRate()   return this->rate;
+    int getQuantity()   return this->quantity;
+};
+
+int main()
+{
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # Super-Market-Billing-System
 Super Market Billing System with File Handling and OOPs using C++.
 
-1. Things that are new for me:  
+1. Things that are new for me: 
+
     1. Why Some Headers Use .h and Some Don’t?
+    
     2. Purpose of using:
         i. iostream
         ii. fstream
         iii. sstream
         iv. windows.h
+
+    3. New way of writing constructor like " Bill(): item(""), rate(0), quantity(0){} "
+
+    4. Defined "Bill" class and added the data member snd member functions to it.
+
+    5. Defined setter and getter funtions inorder to initialize the private data members.
+
+    6. 
+    
