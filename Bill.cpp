7f5d325ac7999d6553a18ce2af05becb0e0d600b
bhupeshsinha <bhupeshsinha247@gmail.com>
2024-11-08 12:35:15 +0530

    we get the user values and then we also assigned this values to the private data members

--- Bill.cpp
+++ Bill.cpp
@@ -36,6 +36,45 @@ class Bill
     int getQuantity()   return this->quantity;
 };
 
+
+
+    addItem(Bill b)
+    {
+        bool close = false;
+        while(!close)
+        {
+            int choice;
+            cout<<"\t1. Add Item."<<endl;
+            cout<<"\t2. Close."<<endl;
+            cout<<"\t Enter Choice: ";
+            cin>>choice;
+
+            if(choice == 1)
+            {
+
+                string item;
+                int rate, quantity;
+
+                cout<<"\t Enter Item Name: ";
+                cin>>item;
+                b.setItem(item);
+
+                cout<<"\t Enter Rate: ";
+                cin>>rate;
+                b.setRate(rate);
+
+                cout<<"\t Enter Quantity: ";
+                cin>>quantity;
+                b.setQuantity(quantity);
+
+            }else{
+
+            }
+        }
+    }
+
+
+
 int main()
 {
     Bill b;
@@ -49,12 +88,23 @@ int main()
 
         cout<<"\t Welcome to Super Market Billing System"<<endl;
         cout<<"\t **************************************"<<endl;
-        cout<<"\t 1. Add Item."<<endl;
-        cout<<"\t 2. Print Bill."<<endl;
-        cout<<"\t 3. Exit."<<endl;
+        cout<<"\t\t 1. Add Item."<<endl;
+        cout<<"\t\t 2. Print Bill."<<endl;
+        cout<<"\t\t 3. Exit."<<endl;
         cout<<"\t\t Enter Choice: ";
         cin>>val;
 
+
+        if(val == 1)
+        {
+
+        }else if(val == 2)
+        {
+
+        }else
+        {
+
+        }
     }
 
     return 0;
